1659ec9afe2b6b3a3e36652cc103b93178f46f2a
PilarFernandez1304 <ccentepilar@gmail.com>
2019-02-21 17:37:07 -0500

    funciones y objetos mmodificados

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-/* Acá va tu código */
+
+const btnCapcifrar=document.getElementById('btnCapcifrar');
+
+btnCapcifrar.addEventListener('click', CapturarCifer)
+
+function CapturarCifer () {
+    const descReceta=document.getElementById('descReceta').value;
+    const offset=document.getElementById('offset').value
+    const obtenerCipher=cipher.encode(offset,descReceta);
+}
+
+//const textReceta= descReceta.toUpperCase();
