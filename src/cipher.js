2d010a5a0e7cff765956f503c09d609bcd096a08
PilarFernandez1304 <ccentepilar@gmail.com>
2019-02-22 13:10:30 -0500

    agregado cifrado y cambio html

--- src/cipher.js
+++ src/cipher.js
@@ -1,9 +1,26 @@
 window.cipher = {
   encode: (offset,descReceta) => {
-    console.log(descReceta.split(''));
-
+    let upperCasedReceta = descReceta.toUpperCase();
+    let neword ='';
+    let alfa='abcdefghijklmnopkrstuvwxyz'.toLocaleUpperCase();
+    //console.log(offset,upperCasedReceta);
+    for(let i=0 ; i<upperCasedReceta.length; i++){
+      const letteriter=upperCasedReceta[i];
+      let pos = alfa.indexOf(letteriter);
+      let posActual=(pos+offset)%26;
+      const newletra=alfa[posActual];
+      console.log(newletra);
+      if(letteriter===' '){
+        neword+=letteriter;
+        //console.log(neword);
+        continue;
+      }
+    }
   },
-  decode: () => {
-    /* Acá va tu código */
+  decode: (offset,descReceta) => {
+    let textCifrado=descReceta.toUpperCase();
+
+
+    
   }
-};
+}
--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 
 const btnCapcifrar=document.getElementById('btnCapcifrar');
-
+const descripCipher=document.getElementById('');
+const second=document.getElementById('second');
+    second.style.display
 btnCapcifrar.addEventListener('click', CapturarCifer)
 
 function CapturarCifer () {
     const descReceta=document.getElementById('descReceta').value;
     const offset=document.getElementById('offset').value
-    const obtenerCipher=cipher.encode(offset,descReceta);
+    const obtenerCipher=cipher.encode(parseInt(offset),descReceta);
+
+
 }
 
 //const textReceta= descReceta.toUpperCase();
